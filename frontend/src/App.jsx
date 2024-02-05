9668b1da0b7d06778ea32485501decb897061bc2
Nico Bustelo <nbustelo2002@gmail.com>
2024-02-05 17:03:07 -0300

    add ruoutes and create pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Singup from './pages/Singup';
+import Singin from './pages/Singin';
 
 export default function App() {
-	return <div className="text-red-500">App</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/sing-in" element={<Singin />} />
+				<Route path="/sing-up" element={<Singup />} />
+				<Route path="/profile" element={<Profile />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
