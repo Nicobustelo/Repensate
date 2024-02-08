4affed125bae0a078067e89d6a5321659ccdc21d
Nico Bustelo <nbustelo2002@gmail.com>
2024-02-08 18:55:41 -0300

    create singin API route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/user.model');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const getAuth = async (req, res) => {
 	res.json({
@@ -25,4 +26,31 @@ const singup = async (req, res, next) => {
 	}
 };
 
-module.exports = { getAuth, singup };
+const singin = async (req, res, next) => {
+	console.log('got here 3');
+	const { email, password } = req.body;
+	try {
+		// checks if user exists
+		const validUser = await User.findOne({ email });
+		console.log('validUser', validUser);
+		if (!validUser) return next('User not found');
+		// checks if password is correct
+		const validPassword = bcryptjs.compareSync(password, validUser.password);
+		console.log('validPassword', validPassword);
+		if (!validPassword) return next('wrong credentials');
+		// creates token for user
+		const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+		// remove the password from user
+		const { password: hashedPassword, ...rest } = validUser._doc;
+		// send token to the cookies of the page
+		// this token only works for each session
+		res
+			.cookie('access_token', token, { httpOnly: true })
+			.status(200)
+			.json(rest);
+	} catch (error) {
+		enxt(error);
+	}
+};
+
+module.exports = { getAuth, singup, singin };
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,8 +1,9 @@
 const authRouter = require('express').Router();
-const { getAuth, singup } = require('../controllers/auth.controller');
+const { getAuth, singup, singin } = require('../controllers/auth.controller');
 
 authRouter.get('/', getAuth);
 
 authRouter.post('/singup', singup);
+authRouter.post('/singin', singin);
 
 module.exports = authRouter;
